6d7bc6b86ef421f52e57883f899665f3df0962e6
ms34 <onethreems@gmail.com>
2022-03-24 14:25:59 +0300

    test server

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // run `node index.js` in the terminal
+import express, { json } from 'express';
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+app.use(json());
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'wow!!! how cool is it to have online editor like blitz' });
+});
+app.get('/test', (req, res) => {
+  res.json({ msg: 'what about this?' });
+});
+
+const port = 3001;
+
+app.listen(port, () => console.log(`server running on blitz port ${port}`));
